b3d61b8f8e7cb19934056b662c23cb712de5d681
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2021-06-22 20:55:21 +0530

    svg added

--- src/navigation/bottomNavigation.js
+++ src/navigation/bottomNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Icons} from 'react-native-elements'
 import {
     MapView,
     Messaging,
@@ -13,10 +14,30 @@ class BottomTabs extends React.Component {
     render(){
         return (
             <Tab.Navigator>
-            <Tab.Screen name="MapView" component={MapView} />
-            <Tab.Screen name="Messaging" component={Messaging} />
-            <Tab.Screen name="Featured" component={Featured} />
-            <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen name="MapView" component={MapView} options={{
+                tabBarIcon: ({ color, size, focused }) => (
+                    <Icons name = '' type = "" size = {18} color = {"#FFFFFF"} />
+                )
+                }}  
+            />
+            <Tab.Screen name="Messaging" component={Messaging} options={{
+                tabBarIcon: ({ color, size, focused }) => (
+                    <Icons name = '' type = "" size = {18} color = {"#FFFFFF"} />
+                )
+                }}
+            />
+            <Tab.Screen name="Featured" component={Featured} options={{
+                tabBarIcon: ({ color, size, focused }) => (
+                    <Icons name = '' type = "" size = {18} color = {"#FFFFFF"} />
+                )
+                }}
+            />
+            <Tab.Screen name="Profile" component={Profile} options={{
+                tabBarIcon: ({ color, size, focused }) => (
+                    <Icons name = '' type = "" size = {18} color = {"#FFFFFF"} />
+                )
+                }}  
+            />
             </Tab.Navigator>
         );
     }
